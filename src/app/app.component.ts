f89b212d7afba64ce7de15cda3293566544c4b9b
rajkumar <rajkumar.ponnusamy6095@gmail.com>
2025-06-22 15:01:07 +0530

    version 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  standalone: true,
+  imports: [
+    AboutComponent,
+    ContactComponent,
+    ExperienceComponent,
+    ServicesComponent,
+    SidebarComponent,
+    SkillsComponent,
+    ProjectsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
